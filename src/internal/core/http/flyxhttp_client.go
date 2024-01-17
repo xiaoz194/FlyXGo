e1ca3ca366538c65503fdaed89e32a77c70d2b7e
evanzha <evanzha@tencent.com>
2024-01-17 19:10:12 +0800

    feat: rename

--- src/internal/core/http/flyxhttp_client.go
+++ src/internal/core/http/flyxhttp_client.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-type BaseClient struct {
+type FlyXHttpClient struct {
 	HttpClient *HttpClient
 	Apis       map[string]ApiDef
 }
@@ -24,7 +24,7 @@ const (
 /*
 RetryApi 带重试机制的Api方法 外部均调用此方法!!
 */
-func (baseClient *BaseClient) RetryApi(options RetryApiOptions) (*http.Response, error) {
+func (flyXHttpClient *FlyXHttpClient) RetryApi(options RetryApiOptions) (*http.Response, error) {
 	var resp *http.Response
 	var err error
 	var wg sync.WaitGroup
@@ -37,11 +37,11 @@ func (baseClient *BaseClient) RetryApi(options RetryApiOptions) (*http.Response,
 			switch options.Mode {
 			case NORMAL:
 				logutil.LogrusObj.Infof("json data is: %v", options.JsonData)
-				resp, err = baseClient.api(options.ApisName, options.JsonData, options.Headers, options.Kwargs, options.Ctx)
+				resp, err = flyXHttpClient.api(options.ApisName, options.JsonData, options.Headers, options.Kwargs, options.Ctx)
 			case UNIQUE_PATH:
-				resp, err = baseClient.unique(options.Method, options.Path, options.JsonData, options.Headers, options.Kwargs, options.Ctx)
+				resp, err = flyXHttpClient.unique(options.Method, options.Path, options.JsonData, options.Headers, options.Kwargs, options.Ctx)
 			case POSTBODY:
-				resp, err = baseClient.apiPassByBody(options.ApisName, options.Body, options.Headers, options.Ctx)
+				resp, err = flyXHttpClient.apiPassByBody(options.ApisName, options.Body, options.Headers, options.Ctx)
 			}
 
 			if err == nil {
@@ -66,8 +66,8 @@ func (baseClient *BaseClient) RetryApi(options RetryApiOptions) (*http.Response,
 }
 
 // private func, this func pass by jsonData
-func (baseClient *BaseClient) api(apisName string, jsonData map[string]interface{}, headers map[string]string, kwargs map[string]interface{}, ctx context.Context) (*http.Response, error) {
-	apiDef, ok := baseClient.Apis[apisName]
+func (flyXHttpClient *FlyXHttpClient) api(apisName string, jsonData map[string]interface{}, headers map[string]string, kwargs map[string]interface{}, ctx context.Context) (*http.Response, error) {
+	apiDef, ok := flyXHttpClient.Apis[apisName]
 	if !ok {
 		return nil, fmt.Errorf("api: %s not exist!", apisName)
 	}
@@ -82,11 +82,11 @@ func (baseClient *BaseClient) api(apisName string, jsonData map[string]interface
 	}
 	logutil.LogrusObj.Info("[method]:", apiDef.Method)
 	logutil.LogrusObj.Info("[path]:", apiDef.Path)
-	return baseClient.HttpClient.DoRequest(apiDef.Method, path, jsonData, nil, headers, ctx)
+	return flyXHttpClient.HttpClient.DoRequest(apiDef.Method, path, jsonData, nil, headers, ctx)
 }
 
 // private func, this func pass by custom
-func (baseClient *BaseClient) unique(method string, path string, jsonData map[string]interface{}, headers map[string]string, kwargs map[string]interface{}, ctx context.Context) (*http.Response, error) {
+func (flyXHttpClient *FlyXHttpClient) unique(method string, path string, jsonData map[string]interface{}, headers map[string]string, kwargs map[string]interface{}, ctx context.Context) (*http.Response, error) {
 	if kwargs != nil {
 		var urlParams string
 		for key, value := range kwargs {
@@ -95,18 +95,18 @@ func (baseClient *BaseClient) unique(method string, path string, jsonData map[st
 		path = path + "?" + urlParams
 		path = path[:len(path)-1]
 	}
-	return baseClient.HttpClient.DoRequest(method, path, jsonData, nil, headers, ctx)
+	return flyXHttpClient.HttpClient.DoRequest(method, path, jsonData, nil, headers, ctx)
 }
 
 // private func, this function pass by body(io.Reader) not by jsonData！
-func (baseClient *BaseClient) apiPassByBody(apisName string, body *bytes.Buffer, headers map[string]string, ctx context.Context) (*http.Response, error) {
-	apiDef, ok := baseClient.Apis[apisName]
+func (flyXHttpClient *FlyXHttpClient) apiPassByBody(apisName string, body *bytes.Buffer, headers map[string]string, ctx context.Context) (*http.Response, error) {
+	apiDef, ok := flyXHttpClient.Apis[apisName]
 	if !ok {
 		return nil, fmt.Errorf("api: %s not exist!", apisName)
 	}
 	path := apiDef.Path
 	logutil.LogrusObj.Info("[method]:", apiDef.Method)
 	logutil.LogrusObj.Info("[path]:", apiDef.Path)
-	return baseClient.HttpClient.DoRequest(apiDef.Method, path, nil, body, headers, ctx)
+	return flyXHttpClient.HttpClient.DoRequest(apiDef.Method, path, nil, body, headers, ctx)
 
 }
